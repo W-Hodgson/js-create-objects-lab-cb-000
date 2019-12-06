2d5ce0859120c60a26aaf0829aad89d9260ac064
Will Hodgson <will.hodgson@current-rms.com>
2019-12-06 11:42:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,5 +26,5 @@ function Bird(name, breed) {
 function Fish(name, species, waterConditions) {
   this.name = name;
   this.species = species;
-  this.waterConditions = waterConditions
+  this.waterConditions = waterConditions;
 }
